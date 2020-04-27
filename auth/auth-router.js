512bd9f4ca4ce846c6c194cdd291bcb9a20605ef
raudelf <rawdell@outlook.com>
2020-04-27 16:50:21 -0500

    imported jwt and my secrets to inorporate tokens in my request.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secrets.js');
 
 const router = require('express').Router();
 const Users = require('../data/user-model.js');
@@ -24,7 +26,10 @@ router.post('/login', (req, res) => {
     Users.findBy({username})
         .then(([user]) => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                res.status(200).json({message: `Welcome ${username}`});
+
+                const token = generateToken(user);
+
+                res.status(200).json({message: `Welcome ${username}`, token});
             } else {
                 res.status(401).json({message: 'Invalid Credentials'});
             }
@@ -33,5 +38,18 @@ router.post('/login', (req, res) => {
             res.status(500).json({message: 'There was a problem with the db', err});
         });
 });
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    };
+
+    const options = {
+        expiresIn: '4h'
+    };
+
+    return jwt.sign(payload, secret.jwtSecret, options)
+}
 
 module.exports = router;
